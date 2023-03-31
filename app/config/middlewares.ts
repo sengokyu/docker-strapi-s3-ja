9b6c1124d4e54036bb901c548181aed4745660c8
sengokyu <sengokyu+gh@gmail.com>
2023-03-31 17:55:25 +0900

    Setup provider-upload-s3

--- app/config/middlewares.ts
+++ app/config/middlewares.ts
@@ -1,12 +1,32 @@
-export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+export default ({ env }) => {
+  const s3host = `${env("AWS_BUCKET_NAME")}.s3.${env(
+    "AWS_REGION"
+  )}.amazonaws.com`;
+
+  return [
+    "strapi::errors",
+    // "strapi::security",
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:"],
+            "img-src": ["'self'", "data:", "blob:", s3host],
+            "media-src": ["'self'", "data:", "blob:", s3host],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+  ];
+};
