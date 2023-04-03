024ba3b86193f40e0dd048eba9e5c5fdbc3c8951
sengokyu <sengokyu+gh@gmail.com>
2023-04-03 11:44:48 +0900

    Update plugin settings

--- app/config/plugins.ts
+++ app/config/plugins.ts
@@ -7,12 +7,21 @@ export default ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: env("AWS_REGION"),
-        params: {
-          Bucket: env("AWS_BUCKET_NAME"),
+        baseUrl: env("CDN_URL"),
+        rootPath: env("CDN_ROOT_PATH"),
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_BUCKET_NAME"),
+          },
         },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
